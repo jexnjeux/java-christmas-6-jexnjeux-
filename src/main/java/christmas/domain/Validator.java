9df38fa63e1d668beebaade0b03d1f6b1bc4bb9c
jexnjeux <zieun.na@gmail.com>
2023-11-14 04:51:32 +0900

    feat: 사용자 입력값 유효성 검사하는 Validator 추가

--- src/main/java/christmas/domain/Validator.java
+++ src/main/java/christmas/domain/Validator.java
@@ -1,6 +1,15 @@
 package christmas.domain;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Validator {
+
+    private Validator() {
+    }
+
+    private static final Menu menu = MenuFactory.create();
 
     public static boolean validateDate(String input) {
         try {
@@ -11,4 +20,44 @@ public class Validator {
         }
     }
 
+    private static String[] orderItemParser(String input) {
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .toArray(String[]::new);
+    }
+
+    public static boolean validateOrder(String input) {
+        String[] orderItems = orderItemParser(input);
+        Set<String> uniqueItems = new HashSet<>();
+
+        for (String orderItem : orderItems) {
+            if (!validateOrderItem(orderItem, uniqueItems)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean validateOrderItem(String orderItem, Set<String> uniqueItems) {
+        String[] order = orderItem.split("-");
+
+        if (!menu.containsItem(order[0])) {
+            return false;
+        }
+
+        if (!validateCount(order[1])) {
+            return false;
+        }
+
+        return uniqueItems.add(order[0]);
+    }
+
+    private static boolean validateCount(String input) {
+        try {
+            int count = Integer.parseInt(input);
+            return count > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
