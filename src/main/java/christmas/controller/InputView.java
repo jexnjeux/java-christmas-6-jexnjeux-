6ccf4d3fdf50a5f7905fa03c667ee578d15d5c11
jexnjeux <zieun.na@gmail.com>
2023-11-14 10:22:36 +0900

    fix: 잘못된 날짜 입력한 경우, 출력 메시지 수정

--- src/main/java/christmas/controller/InputView.java
+++ src/main/java/christmas/controller/InputView.java
@@ -13,7 +13,7 @@ public class InputView {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String input = Console.readLine();
         while (!Validator.validateDate(input)) {
-            System.out.println("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
             input = Console.readLine();
         }
         return Integer.parseInt(input);
